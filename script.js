319f89296fc87a0310f466f1a96cfe2fbd99c259
Sam Singer <samuel.singer1@gmail.com>
2017-07-27 01:03:33 -0600

    JS refactor with prioritizaton and HTML ARIA with HTML refactor

--- script.js
+++ script.js
@@ -1,55 +1,33 @@
 $(window).on('load', storageCheck);
-$('.to-do-card-parent').on('click', '#delete', removeCardFromStorage);
 $('.task-input, .detail-input').on('keyup', enableSave);
-$('.to-do-card-parent').on('click', '#upvote', upvote);
-$('.to-do-card-parent').on('click', '#downvote', downvote);
 $('.save-btn').on('click', saveClick);
+$('.search-input').on('keyup', searchCards);
+$('.filter-importance').on('change', filterImportance);
 $('.to-do-card-parent').on('blur', 'h2', editCardTask)
                        .on('blur', '.detail-text', editCardDetail);
-$('.search-input').on('keyup', searchCards);
-$('.to-do-card-parent').on('click', '.completed-task-btn', taskComplete);
+$('.to-do-card-parent').on('click', '#upvote', upvote)
+                       .on('click', '#downvote', downvote)
+                       .on('click', '#delete', removeCardFromStorage)
+                       .on('click', '.completed-task-btn', taskComplete);
 $('.show-btn').on('click', showBtn);
-$('.filter-importance').on('change', filterImportance);
 
-function CardElements(title, body) {
-  this.title = title;
-  this.body = body;
-  this.id = Date.now();
-  this.completed = false;
-  this.importance = 'normal';
+function storageCheck() {
+  var cardArray = [];
+  retrieveLocalStorage();
+  limitCardList();
+  clearInputs();
 };
 
 function enableSave() {
   if (($('.task-input').val() !== "") || ($('.detail-input').val() !== "")) {
     $('.save-btn').removeAttr('disabled');
-  }
+  };
 };
 
 function saveClick(event) {
   event.preventDefault();
   fireCards();
   $('.save-btn').attr('disabled', 'disabled');
-};
-
-function addCards(buildCard) {
-  if (buildCard.completed === true) {
-    var grayOut = "grayout";
-  }
-  $('.to-do-card-parent').prepend(
-    `<article class="to-do-card ${grayOut}" id="${buildCard.id}">
-      <h2 contenteditable="true">${buildCard.title}</h2>
-      <aside class="complete-delete-container">
-        <div class="completed-task-btn"></div>
-        <div class="delete-btn" id="delete"></div>
-      </aside>
-        <p class="detail-text" contenteditable="true">${buildCard.body}</p>
-      </div>
-      <div class="ratings">
-        <div class="upvote-btn" id="upvote"></div>
-        <div class="downvote-btn" id="downvote"></div>
-        <p class="importance">Importance: <span class="${buildCard.id}">${buildCard.importance}</span></p>
-
-    </article>`);
 };
 
 function fireCards() {
@@ -61,40 +39,95 @@ function fireCards() {
   limitCardList();
 };
 
+function CardElements(title, body) {
+  this.title = title;
+  this.body = body;
+  this.id = Date.now();
+  this.completed = false;
+  this.importance = 'normal';
+};
+
+function addCards(buildCard) {
+  if (buildCard.completed === true) {
+    var grayOut = "grayout";
+  }
+  $('.to-do-card-parent').prepend(
+    `<article class="to-do-card ${grayOut}" id="${buildCard.id}">
+    <h2 contenteditable="true">${buildCard.title}</h2>
+    <aside class="complete-delete-container">
+    <div class="completed-task-btn"></div>
+    <div class="delete-btn" id="delete"></div>
+    </aside>
+    <p class="detail-text" contenteditable="true">${buildCard.body}</p>
+    </div>
+    <div class="ratings">
+    <div class="upvote-btn" id="upvote"></div>
+    <div class="downvote-btn" id="downvote"></div>
+    <p class="importance">Importance: <span class="${buildCard.id}">${buildCard.importance}</span></p>
+
+    </article>`);
+  };
+
 function storeCards() {
   localStorage.setItem('array', JSON.stringify(cardArray));
   clearInputs()
 };
 
-function retrieveLocalStorage() {
-  console.log('hey');
-  cardArray = JSON.parse(localStorage.getItem('array')) || [];
-  cardArray.forEach(function(card) {
-
-    //hideComplete();
-    addCards(card);
-  })
-};
-
-// function hideComplete() {
-//     cardArray.forEach(function(card) {
-//         if (card.complete === true) {
-//           card.hide();
-//         }
-//     })
-    //go into cardArray, loop through each el and get completed = true, .hide() those
-//}
-
 function clearInputs() {
   $('.task-input').val('');
   $('.detail-input').val('');
+  $('.task-input').focus();
 };
 
-function storageCheck() {
-  var cardArray = [];
-  retrieveLocalStorage();
-  limitCardList();
-  clearInputs();
+function searchCards() {
+  var search = $(".search-input").val().toUpperCase();
+  var results = cardArray.filter(function(elementCard) {
+    return elementCard.title.toUpperCase().includes(search) ||
+    elementCard.body.toUpperCase().includes(search);
+  });
+  addCardsBack(results);
+};
+
+function filterImportance(event) {
+  var selectedImportance = event.target.value;
+  var results;
+  if (selectedImportance === 'all') {
+    results = cardArray.slice();
+  } else {
+    results = cardArray.filter(function(elementCard) {
+      return elementCard.importance === selectedImportance;
+    });
+  };
+  addCardsBack(results);
+};
+
+function addCardsBack(results) {
+  $('.to-do-card-parent').empty();
+  results.forEach(function(card) {
+    addCards(card);
+  });
+};
+
+function editCardTask() {
+  var id = $(this).closest('.to-do-card')[0].id;
+  var title = $(this).text();
+  cardArray.forEach(function(card) {
+    if (card.id == id) {
+      card.title = title;
+    };
+  });
+  storeCards();
+};
+
+function editCardDetail() {
+  var id = $(this).closest('.to-do-card')[0].id;
+  var body = $(this).text();
+  cardArray.forEach(function(card) {
+    if (card.id == id) {
+      card.body = body;
+    };
+  });
+  storeCards();
 };
 
 function upvote() {
@@ -102,13 +135,13 @@ function upvote() {
   var importance = ['none', 'low', 'normal','high','critical']
   cardArray.forEach(function(card) {
     if (card.id == cardId) {
-    var currentIndex = importance.indexOf(card.importance);
-    currentIndex = (currentIndex !== 4) ? currentIndex + 1 : currentIndex;
-    card.importance = importance[currentIndex];
+      var currentIndex = importance.indexOf(card.importance);
+      currentIndex = (currentIndex !== 4) ? currentIndex + 1 : currentIndex;
+      card.importance = importance[currentIndex];
       $('.' + cardId).text(card.importance);
-    }
+    };
     storeCards();
-  })
+  });
 };
 
 function downvote() {
@@ -120,82 +153,29 @@ function downvote() {
       currentIndex = (currentIndex !== 0) ? currentIndex - 1 : currentIndex;
       card.importance = importance[currentIndex];
       $('.' + cardId).text(card.importance);
-      }
-      storeCards();
-    })
-  };
-
-function saveClick(event) {
-  event.preventDefault();
-  fireCards();
-  $('.save-btn').attr('disabled', 'disabled');
-};
-
-function editCardTask() {
-  console.log(this);
-  var id = $(this).closest('.to-do-card')[0].id;
-  var title = $(this).text();
-  cardArray.forEach(function(card) {
-    if (card.id == id) {
-      card.title = title;
-    }
-  })
-  storeCards();
-};
-
-function editCardDetail() {
-  console.log(this);
-  var id = $(this).closest('.to-do-card')[0].id;
-  var body = $(this).text();
-  cardArray.forEach(function(card) {
-    if (card.id == id) {
-      card.body = body;
-    }
-  })
-  storeCards();
-};
-
-function searchCards() {
-  console.log("searchCards");
-  var search = $(".search-input").val().toUpperCase();
-  var results = cardArray.filter(function(elementCard) {
-    return elementCard.title.toUpperCase().includes(search) ||
-           elementCard.body.toUpperCase().includes(search);
+    };
+    storeCards();
   });
-  addCardsBack(results);
 };
 
-function filterImportance(event) {
-  var selectedImportance = event.target.value;
-  var results;
-  if (selectedImportance === 'all') {
-    results = cardArray.slice();
-  } else {
-  results = cardArray.filter(function(elementCard) {
-    return elementCard.importance === selectedImportance;
-    });
-  }
-    addCardsBack(results);
-}
-
-function addCardsBack(results) {
-  $('.to-do-card-parent').empty();
-  results.forEach(function(card) {
-    addCards(card);
+function removeCardFromStorage() {
+  var currentCardId = $(this).closest('.to-do-card')[0].id
+  cardArray.forEach(function(card, index) {
+    if (currentCardId == card.id) {
+      cardArray.splice(index, 1);
+    };
   });
-}
+  storeCards();
+  $(this).parents('.to-do-card').remove();
+};
 
-// We now need the class to persist!
-// Loop through the array similar to the upvote/donwvote, look for a matching card in localStorage
-// Once you have that card you can change the object .thatCompleted
-// Put back into localStorage
 function taskComplete() {
   console.log("it works")
   var cardId = $(this).closest('.to-do-card')[0].id;
   cardArray.forEach(function(card) {
-  if (card.id == cardId) {
-    card.completed = true;
-    }
+    if (card.id == cardId) {
+      card.completed = true;
+    };
   });
   $(this).parent().parent().addClass('grayout');
   storeCards();
@@ -205,24 +185,29 @@ function storeCards() {
   localStorage.setItem('array', JSON.stringify(cardArray));
 };
 
-function clearInputs() {
-  $('.task-input').val('');
-  $('.detail-input').val('');
-  $('.task-input').focus();
+function showBtn() {
+  $('.to-do-card-parent').empty();
+  toggleBtnText();
 };
 
-function removeCardFromStorage() {
-  var currentCardId = $(this).closest('.to-do-card')[0].id
-  cardArray.forEach(function(card, index) {
-    if (currentCardId == card.id) {
-      cardArray.splice(index, 1);
-    }
+function toggleBtnText() {
+  var $showBtn = $('.show-btn');
+  if ($showBtn.text() === 'Show more...') {
+    $showBtn.text('Show less...');
+    retrieveLocalStorage();
+  } else { $showBtn.text('Show more...');
+    $('.to-do-card-parent').empty();
+    limitCardList();
+  };
+};
+
+function retrieveLocalStorage() {
+  cardArray = JSON.parse(localStorage.getItem('array')) || [];
+  cardArray.forEach(function(card) {
+    addCards(card);
   });
-  storeCards();
-  $(this).parents('.to-do-card').remove();
 };
 
-//SHOW MORE/LESS FUNCTIONALITY
 function limitCardList(card) {
   var splicedCards  = [];
   if(cardArray.length > 10) {
@@ -230,30 +215,13 @@ function limitCardList(card) {
     $('.to-do-card-parent').empty();
     for (var i = 0; i < cardArray.length; i++) {
       addCards(cardArray[i]);
-    }
-  }
+    };
+  };
   return splicedCards;
-}
-
-function showBtn() {
-  $('.to-do-card-parent').empty();
-  toggleBtnText();
-}
-
-function toggleBtnText() {
-  var $showBtn = $('.show-btn');
-  if ($showBtn.text() === 'Show more...') {
-    $showBtn.text('Show less...');
-    retrieveLocalStorage();
-  } else {
-    $showBtn.text('Show more...');
-    $('.to-do-card-parent').empty();
-    limitCardList();
-  }
-}
+};
 
 function disableShowMore() {
-  if (cardArray.length <= 10) {
+  if (cardArray.length >= 10) {
     $('.show-btn').attr('disabled', true);
-  }
-}
+  };
+};
